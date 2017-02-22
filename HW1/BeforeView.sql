a52d216b667fe566222b7bf0c20ee101b04d06ee
ATTron <twantempleton11@gmail.com>
2017-02-22 11:22:23 -0500

    true 11 fix

--- HW1/BeforeView.sql
+++ HW1/BeforeView.sql
@@ -8,7 +8,8 @@ select DISTINCT c.first_name, c.last_name from customer c right join product_ord
 select product_name from product where product_name like '%Pole';
 
 -- 11
-select product_name, COUNT(o.product_id) as num_reorder from product p, order_details o where p.product_id = o.product_id group by product_name;
+select product_name, COUNT(o.product_id) as num_reorder from product p left join order_details o on p.product_id = o.product_id group by product_name;
+--select product_name, COUNT(o.product_id) as num_reorder from product p, order_details o where p.product_id = o.product_id group by product_name;
 
 -- 14
 select product_id, product_name, (quantity_in_stock - reorder_point) as remaining_stock from product;
